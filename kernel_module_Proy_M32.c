ceaa8a95830ca213875e3d6bdfa92330862235df
husein-gonzalez <husein.gomiz@gmail.com>
2018-12-16 17:55:18 +0100

    timer fpga sin crear nueva direccion virtual

--- kernel_module_Proy_M32.c
+++ kernel_module_Proy_M32.c
@@ -10,14 +10,14 @@
 #define HPS_TIMER2_SPAN 0X14
 #define HPS_TIMER3_BASE 0XFFD01000
 #define HPS_TIMER3_SPAN 0X14
-#define FPGA_INTERVAL_TIMER_BASE 0xFF202000
-#define FPGA_INTERVAL_TIMER_SPAN 0X20
+//#define FPGA_INTERVAL_TIMER_BASE 0xFF202000
+//#define FPGA_INTERVAL_TIMER_SPAN 0X20
 #define HPS_TIMER2_IRQ 201
 #define HPS_TIMER3_IRQ 202
 #define FPGA_INTERVAL_TIMER_IRQ 72
 
 void * LW_virtual; // Lightweight bridge base address
-volatile int *LEDR_ptr, *HPS_Timer2_ptr, *HPS_Timer3_ptr, *FPGA_Timer_Interval_ptr; // virtual addresses
+volatile int *LEDR_ptr, *HPS_Timer2_ptr, *HPS_Timer3_ptr, *FPGA_Timer_Interval_ptr, *JP2_ptr; // virtual addresses
 void * HPS_Timer2_virtual; // Timer2 base address
 void * HPS_Timer3_virtual; // Timer3 base address
 void * FPGA_Timer_Interval_Virtual; // Fpga Timer base address
@@ -28,7 +28,14 @@ void * FPGA_Timer_Interval_Virtual; // Fpga Timer base address
  {
 	 
  int value, value2;
- *LEDR_ptr = *LEDR_ptr ^ 0x1;
+ //*LEDR_ptr = *LEDR_ptr ^ 0x1;
+ 
+ //mover servoX
+ 
+ 
+ 
+ 
+ 
   value = *(HPS_Timer2_ptr+3); //borrar flag
 
 
@@ -80,18 +87,20 @@ void * FPGA_Timer_Interval_Virtual; // Fpga Timer base address
  LEDR_ptr = LW_virtual + LEDR_BASE; // virtual address for LEDR port
  *LEDR_ptr = 0x001; // turn on the righttmost light
  
+ JP2_ptr = LW_virtual + JP2_BASE; // virtual address for GPIO1 port
+ 
  //crea direcciones virtuales de los timers
  HPS_Timer2_virtual = ioremap_nocache (HPS_TIMER2_BASE, HPS_TIMER2_SPAN); 
  HPS_Timer3_virtual = ioremap_nocache (HPS_TIMER3_BASE, HPS_TIMER3_SPAN);
- FPGA_Timer_Interval_Virtual = ioremap_nocache (FPGA_INTERVAL_TIMER_BASE, FPGA_INTERVAL_TIMER_SPAN);
+ //FPGA_Timer_Interval_Virtual = ioremap_nocache (FPGA_INTERVAL_TIMER_BASE, FPGA_INTERVAL_TIMER_SPAN);
  
  
  //configuracion timer2. reloj de 25MHz
- HPS_Timer2_ptr = HPS_Timer2_virtual; //salta cada segundo
+ HPS_Timer2_ptr = HPS_Timer2_virtual; //salta cada microsegundo
  
    *(HPS_Timer2_ptr + 2) = 0x00FE;  //deshabilita timer
    *(HPS_Timer2_ptr + 3) = 0X00FE;  // borra flag
-   *HPS_Timer2_ptr = 25000000;		//Load Value
+   *HPS_Timer2_ptr = 25;		//Load Value
    *(HPS_Timer2_ptr + 2) = 0x0003;   //Auto Reload & Enable
    
 //configuracion timer3. reloj de 25MHz
@@ -105,7 +114,9 @@ HPS_Timer3_ptr = HPS_Timer3_virtual; //salta cada dos segundos
    
    
 //configuracion timer FPGA Interval. reloj de 100MHz
-FPGA_Timer_Interval_ptr = FPGA_Timer_Interval_Virtual; //salta cada tres segundos
+//FPGA_Timer_Interval_ptr = FPGA_Timer_Interval_Virtual; //salta cada tres segundos
+ 
+ FPGA_Timer_Interval_ptr = LW_virtual + TIMER_BASE;
  
    *(FPGA_Timer_Interval_ptr + 1) = *(FPGA_Timer_Interval_ptr + 1) | 0x8; //parar contador
    *(FPGA_Timer_Interval_ptr + 1) = *(FPGA_Timer_Interval_ptr + 1) | 0x2;  //set auto reload (CONT)
